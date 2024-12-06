4dc313911c1b9da8a1b56ab353d5ff38e33d9ac3
Aidan Boling <aidanboling@gmail.com>
2024-12-05 22:27:25 -0800

    Minor refactor. Add pre-conversion string formatting.

--- D83 - Morse Code Converter/main.py
+++ D83 - Morse Code Converter/main.py
@@ -1,10 +1,13 @@
 import sys
+import re
 import lib
 
 # Text-based (command line) program that takes any String input and converts it into Morse Code.
+# Currently only converts English alphabet.
 
 ENCODE_DICT = lib.morse_code_dict
 DECODE_DICT = {value: key for key, value in ENCODE_DICT.items()}
+
 
 def main():
 
@@ -12,8 +15,11 @@ def main():
         action = get_input('Type "encode" to convert TO Morse Code, type "decode" to convert FROM Morse Code: \n', 'options', ['decode', 'encode'])
         message =  get_input('Type the message: \n')
 
-        result = transform_message(action, message)
-        print(f"Here's the converted message: \n{result}")
+        converted_msg, ignored = convert_message(action, message)
+
+        print(f"Here's the converted message: \n{converted_msg}")
+        if len(ignored):
+            print(f'\nThe following characters were ignored: \n', ignored)
 
         if input('Convert another message? (y/n) ').strip().lower() != 'y':
             break
@@ -53,34 +59,48 @@ def confirm_exit():
 
     else:
         sys.exit()
+   
+
+def preformat_msg(action: str, message: str):   
+    formatted_msg = message.strip().upper()
+
+    if action == 'encode':
+        formatted_msg = re.sub("\.+|!", ' STOP', formatted_msg)     # Change end-of-sentence punctuation '...', '.', '!' to 'STOP'
+        formatted_msg = re.sub("\s\s+", ' ', formatted_msg)     # Multiple spaces => only one space
+        
+    return formatted_msg
 
 
-def transform_message(action, message):
-    transformed_msg = ''
+def convert_message(action, message):
+    converted_msg = ''
+    ignored_chars = []
 
-    
-    # shifted_list = get_shifted_list(shift)
-    # start_list = message
-    # lookup_list = ENCODE_DICT
+    conversion_dict = ENCODE_DICT
 
     if action == 'decode':
-        pass
-        # start_list = DECODE_DICT
-        # lookup_list = get_shifted_list(shift)
+        conversion_dict = DECODE_DICT
 
     for char in message:
-        # if char in chars:
-        #     i = start_list.index(char)
-        #     transformed_msg += lookup_list[i]
+        # if char in conversion_dict:
+        #     i = message.index(char)
+        #     transformed_msg += lookup_dict[i]
 
         # else:
         #     transformed_msg += char
 
-    return transformed_msg
+        if char in conversion_dict:
+            pass
 
+        else:
+            ignored_chars.append(char)
 
+    return converted_msg, ignored_chars
 
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
