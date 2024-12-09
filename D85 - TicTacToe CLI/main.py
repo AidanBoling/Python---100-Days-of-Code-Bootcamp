b09f223d64d3bde1ce77224af25a24ed5b0bc31f
Aidan Boling <aidanboling@gmail.com>
2024-12-08 18:43:24 -0800

    Add to GameLogic: user input processing, get next move, new-game setup. Also, misc. small fixes.

--- D85 - TicTacToe CLI/main.py
+++ D85 - TicTacToe CLI/main.py
@@ -1,3 +1,7 @@
+import sys
+import re
+import random
+
 def main():
 
     # Initial setup user questions:
@@ -14,14 +18,64 @@ def main():
     pass
 
 
+# Helpers:
+
+def confirm_exit(noun:str = 'program'):
+        confirm = input(f'No input given. Exit {noun}? (y/n) ')
+        
+        if confirm.strip().lower() == 'n':
+            return
+
+        else:
+            print('Exiting.')
+            sys.exit()
+
+
+def get_input(prompt, options:list = [], validation_func=None):
+        '''Asks for input from user, using passed-in prompt. If any options
+        passed in, only input matching one of the options is accepted. If 
+        input is empty, user asked if they would like to exit.
+        
+        Also accepts a validation_func: any function that accepts a string
+        (the user input) and returns a boolean, where True means the string 
+        is valid.
+        '''
+
+        while True:
+            try:
+                user_input = input(prompt).strip()
+                if user_input:
+                    if len(options) > 0 or validation_func:
+                        if len(options) > 0 and user_input.lower() in options:
+                            return user_input.lower()
+                        
+                        elif validation_func(user_input):
+                            return user_input.lower()
+                        
+                        else:
+                            print('\nInvalid entry. \n')
+
+                    else:
+                        return user_input.lower()
+
+                else:
+                    confirm_exit('game')
+
+            except KeyboardInterrupt:
+                print('\n')
+                sys.exit()
+
+
+# PLAYER
 class Player:
-    def __init__(self):
+    def __init__(self, name:str):
         self.marker = None
         self.wins = 0
+        self.name:str = name
 
 
 
-# Board
+# BOARD
 
 COL_LABELS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
@@ -33,6 +87,7 @@ class Board:
         self.empty_slot = ' '
         self._slot_matrix:list = []
         self._slots:dict = {}
+        self._marker_options = ['X', 'O']       # marker order matches turn order
 
         self.build_board()
 
@@ -54,7 +109,7 @@ class Board:
         cell_padding = ' ' * int((cell_width - 1) / 2)
         col_label_cell_padding = ' ' * cell_width
 
-        col_labels_row = f'{(" " * row_start_spaces)} ' + cell_padding + f'{col_label_cell_padding}'.join(self.cols)
+        col_labels_row = f'{(" " * row_start_spaces)}' + cell_padding + f'{col_label_cell_padding}'.join(self.cols)
         top_border_row = (' ' * row_start_spaces) + (('|' + cell_border) * self.size) + '|'
         border_row = (' ' * row_start_spaces) + '|' + ((cell_border + '+') * (self.size - 1)) + cell_border + '|'
         bottom_border_row = (' ' * row_start_spaces) + ' ' + ((cell_border + '-') * (self.size - 1)) + cell_border
@@ -79,33 +134,58 @@ class Board:
     
 
     def place_marker(self, marker:str, column:str, row:int):
-        self._slots[column][row] = marker
-        
-        # Should throw index (?) error if row or column is outside of board
+        slot_selected = self._slots[column][row]    # Adding extra step so throws KeyError if *either* 'column' or 'row' are invalid (rather than just 'column')
+        if slot_selected == self.empty_slot:
+            self._slots[column][row] = marker
+        else:
+            raise ValueError('Invalid move: Slot already taken.\n')
+    
 
-    # TODO (?): throw error if try to instantiate with board size larger than LETTERS
+    def clear_board():
+        #TODO
+        pass
 
 
+    # TODO (?): throw error if try to instantiate with board size larger than COL_LABELS
+
+
+
+
+#GAME BRAIN
 
 class GameLogic:
     def __init__(self, board: Board, player_a: Player, player_b: Player):
         self.board = board
-        self.player_a = player_a
-        self.player_b = player_b
+        self.marker_options = self.board._marker_options
+        self.player_a: Player = player_a
+        self.player_b: Player = player_b
         self.player_current_turn: Player = None
         self.winner_last_game: Player = None
         self.total_games = 0
         self.game_over = True
         
+        self.setup_new_game()
 
     def setup_new_game(self):
-        # assign markers to players:
-        #       - random if total_games == 0
-        #       - otherwise, whoever didn't win
-        
-        # Set current_player_turn -- whoever assigned "X"
-        pass
-    
+        players = [self.player_a, self.player_b]
+
+        is_first = None
+        if self.total_games == 0:
+            is_first = random.choice([self.player_a, self.player_b])
+            
+        else:
+            for player in players:
+                if player != self.winner_last_game:
+                    is_first = player
+
+        players.remove(is_first)
+        players.insert(0, is_first)
+
+        for i in range(2):
+            players[i].marker = self.marker_options[i]
+
+        self.player_current_turn = players[0]
+
 
     def run_game(self):
         self.setup_new_game()
@@ -136,29 +216,62 @@ class GameLogic:
                 
     
     def switch_player_turn(self):
-        players = [self.player_a, self.player_b]
-        for player in players:
+        for player in [self.player_a, self.player_b]:
             if self.player_current_turn != player:
                 self.player_current_turn = player
 
 
     def get_player_move(self):
-        #   Get user input
-            #   - only accept strings of length 2, alphanumeric, containing 1 number, and 1 alpha.
+        '''Takes input from user, and enters onto board. If input not valid at 
+        any point, prompts player for input again.'''
 
-        # update board upon input
-            # split input into appropriate parts (column is alpha, row is number) 
-            # try/catch self.board.place_marker -- If board returns index error, tell user that it is invalid, 
-                                                #  If no error, break input loop
-        pass
-    
-    
+        player = self.player_current_turn
+        marker = player.marker
+        column = None
+        row = None
+
+
+        def move_validation(input:str):            
+            if not len(input)==2:
+                return
+            if not input.isalnum():
+                return
+            if not (re.search('[a-zA-Z]+', input) and re.search('[0-9]', input)):
+                return
+            
+            return True
+        
+
+        marker_placed = False
+        while not marker_placed:
+            player_prompt = f"{player.name}'s move: "
+            
+            player_move = get_input(player_prompt, validation_func=move_validation)
+            
+            coordinates = list(player_move)
+            for coord in coordinates:
+                if coord.isalpha():
+                    column = coord.upper()
+                if coord.isnumeric():
+                    row = int(coord)
+
+            try:
+                self.board.place_marker(marker, column, row)
+                marker_placed = True
+            except KeyError:
+                print('Invalid move -- outside of board. Please try again.')
+                print(f'Valid column letters: {self.board.cols}\nValid row numbers: {self.board.rows}\n')
+            except ValueError as e:
+                print(e)
+
     def display_updated_board(self):
         img = self.board.generate_board_img()
         print('\n', img, '\n')
         
 
     def check_for_win(self):
+        '''Determine if there has been a win -- any row, column, 
+        or diagonal with the same marker filling all slots.'''
         vectors = []
         row_nums = self.board.rows
         col_labels = self.board.cols
@@ -184,7 +297,6 @@ class GameLogic:
             
             diagonals[0].append(slots[col][row])
             diagonals[1].append(slots[col][row_rev])
-        
         vectors.extend(diagonals)
 
         for vector in vectors:
@@ -195,7 +307,6 @@ class GameLogic:
         
         print('No wins')
             
-
 
 if __name__ == "__main__":
     main()
